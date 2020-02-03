1894c726b0d6214cec8250e6ebf542210c3e03db
RabbitRPA <59790840+RabbitRPA@users.noreply.github.com>
2020-02-03 17:40:44 +0100

    Update czwicenia.py

--- czwicenia.py
+++ czwicenia.py
@@ -1,3 +1,5 @@
+"""
+
 x = 42
 y = 5.2
 z = "siema"
@@ -7,3 +9,7 @@ print(type(x))
 print(type(y))
 print(type(z))
 print(type(p))
+"""
+
+x = int(input("Wprowadż liczbę: "))
+y = int(input("Wprowadż liczbę: "))
